18cfbd8134160391ac473d995ad570d9c2534d69
Parikshit Gehlaut <parikshitgehlaut222@gmail.com>
2024-12-05 11:15:27 +0530

    commiting staged changes in Navbar.

---
+++ src/routes/RegisterPage.jsx
@@ -0,0 +1,14 @@
+import { SignUp } from "@clerk/clerk-react";
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center h-[calc(100vh - 80px)]">
+        <SignUp signInUrl="/login" />
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
---
+++ src/routes/SinglePageList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SinglePageList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SinglePageList
